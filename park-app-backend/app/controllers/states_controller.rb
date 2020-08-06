beb2cab1fcbb687c56b677f868fd0fce9fc6cfb1
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 18:10:09 -0400

    Add create action and strong params permissions for states

--- park-app-backend/app/controllers/states_controller.rb
+++ park-app-backend/app/controllers/states_controller.rb
@@ -9,12 +9,21 @@ class StatesController < ApplicationController
         render json: state, include: [:parks => {except: excluded_data}], except: excluded_data
     end
 
+    def create
+        state = State.create(state_params)
+        redirect_to state_path(state)
+    end
+
     def destroy
         state = State.find(params[:id])
         state.destroy()
     end
 
     private
+
+    def state_params
+        params.permit(:name)
+    end
 
     def excluded_data
         [:created_at, :updated_at]
