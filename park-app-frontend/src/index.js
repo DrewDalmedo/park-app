652c18a6dcf4354f25120fc5eda3ed762716681f
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 15:48:33 -0400

    Fetch and render states

--- park-app-frontend/src/index.js
+++ park-app-frontend/src/index.js
@@ -3,21 +3,51 @@ const STATES_URL = `${BASE_URL}/states`
 const PARKS_URL = `${BASE_URL}/parks`
 
 
+let stateContainer = document.querySelector("#state-container")
+
 function getStates() {
-    // fill me out once you have seed data!
     fetch(STATES_URL)
     .then( response => {
-        response.json()
-        .then( json => {
-            json.forEach(renderState)
-        })
+        return response.json()
+    })
+    .then( json => {
+        json.forEach(renderState)
     })
 }
 
-function renderState() {
+function renderState(state) {
     // add more stuffs
+    stateContainer.innerHTML += `
+    <div data_id=${state.id}><p>${state.name}</p>
+        <button class="delete-button" data-state-id=${state.id}>Delete State</button>
+        <ul id="state-${state.id}-park-list">
+            ${state.parks.map(makeParkLi).join("")}
+        </ul>
+        <p>=======================</p>
+    </div>
+    `
+
 }
 
+function makeParkLi(park) {
+    return `<li>${park.name}</li>`
+}
+
+// on loading the page's content
 document.addEventListener("DOMContentLoaded", () => {
     getStates();
 })
+
+// on clicking the delete button of a state
+stateContainer.addEventListener("click", (e) => {
+    if (e.target.className === "delete-button") {
+        e.target.disabled = true;
+        let stateID = e.target.dataset.stateId
+
+        fetch(`http://localhost:3000/state/${stateID}`, {
+
+        })
+
+    }
+})
+
