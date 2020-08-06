0996f7cd713d1a88a086eb13061231e797020f16
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 13:47:40 -0400

    Add foundation for getting states from backend

--- park-app-frontend/src/index.js
+++ park-app-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const STATES_URL = `${BASE_URL}/states`
 const PARKS_URL = `${BASE_URL}/parks`
+
+
+function getStates() {
+    // fill me out once you have seed data!
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getStates();
+})
