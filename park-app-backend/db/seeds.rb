722e77e5df7ea02a96c99097372285b4ee5767e6
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 13:48:02 -0400

    Add seeds for testing

--- park-app-backend/db/seeds.rb
+++ park-app-backend/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# new york, cali, nevada
+ny = State.create(name: 'New York')
+cali = State.create(name: 'California')
+nv = State.create(name: 'Nevada')
+
+# new york parks:
+Park.create(name: 'Appalachian Trail', state: ny)
+Park.create(name: 'Chesapeake Bay', state: ny)
+Park.create(name: 'Ellis Island', state: ny)
+
+# california parks:
+Park.create(name: 'Alcatraz Island', state: cali)
+Park.create(name: 'Death Valley', state: cali)
+Park.create(name: 'Golden Gate Bridge', state: cali)
+
+# nevada parks:
+Park.create(name: 'Great Basin', state: nv)
+Park.create(name: 'Lake Mead', state: nv)
+Park.create(name: 'Tule Springs Fossil Beds', state: nv)
