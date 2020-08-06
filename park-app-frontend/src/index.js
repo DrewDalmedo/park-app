44fe2aec00e60c1bb260f1cb6a7b57aed2a6ac77
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 14:17:45 -0400

    Add response handling

--- park-app-frontend/src/index.js
+++ park-app-frontend/src/index.js
@@ -5,6 +5,17 @@ const PARKS_URL = `${BASE_URL}/parks`
 
 function getStates() {
     // fill me out once you have seed data!
+    fetch(STATES_URL)
+    .then( response => {
+        response.json()
+        .then( json => {
+            json.forEach(renderState)
+        })
+    })
+}
+
+function renderState() {
+    // add more stuffs
 }
 
 document.addEventListener("DOMContentLoaded", () => {
