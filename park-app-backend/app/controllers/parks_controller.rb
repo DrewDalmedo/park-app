cdb70f2d90db4972c73b5d9304c09575484f81aa
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-06 14:13:19 -0400

    Render index and show json for parks

--- park-app-backend/app/controllers/parks_controller.rb
+++ park-app-backend/app/controllers/parks_controller.rb
@@ -1,2 +1,17 @@
 class ParksController < ApplicationController
+    def index
+        parks = Park.all
+        render json: parks, except: excluded_data
+    end
+
+    def show
+        park = Park.find([params[:id]])
+        render json: park, except: excluded_data
+    end
+
+    private
+
+    def excluded_data
+        [:created_at, :updated_at]
+    end
 end
