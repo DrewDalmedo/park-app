570adc582a2d6109ff6521fc435a3992c31de20f
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-13 16:53:21 -0400

    Create Park class

---
+++ park-app-frontend/src/park.js
@@ -0,0 +1,28 @@
+class Park {
+    static all = []
+
+    constructor({id, name, state_id}) {
+        this.id = id;
+        this.name = name;
+        this.state_id = state_id;
+
+        this.main = document.createElement("li")
+
+        Park.all.push(this)
+    }
+
+    static getByState(id) {
+
+        let matchArray = []
+
+        Park.all.forEach( (park) => {
+            if (park.state_id === id) {
+                matchArray.push(park)
+            }
+        })
+
+        return matchArray
+    }
+
+
+}
