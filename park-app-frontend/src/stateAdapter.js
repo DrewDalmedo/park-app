db1be18993af351dd40649596fa9a8f9a01e6ebe
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-13 17:21:21 -0400

    Add createState method for state creation

--- park-app-frontend/src/stateAdapter.js
+++ park-app-frontend/src/stateAdapter.js
@@ -17,6 +17,21 @@ class StateAdapter {
         .then( (response) => response.json())
     }
 
+    createState(info) {
+        fetch( this.baseURL, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: info[0]
+            })
+        })
+        .then( response => response.json() )
+        .then( stateJSON => new State(stateJSON).render() )
+    }
+
     deleteState(stateID) {
         fetch(`http://localhost:3000/states/${stateID}`, {
             method: "DELETE"
