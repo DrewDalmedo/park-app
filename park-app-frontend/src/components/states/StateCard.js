dbacb0c5d4ff05f1ad3c204bcf0b03864fcb8461
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-29 17:21:58 -0400

    Create boilerplate components for States and StateCards

---
+++ park-app-frontend/src/components/states/StateCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class StateCard extends React.Component {
+  render() {
+    return (
+      <div>
+        StateCard Component
+      </div>
+    )
+  }
+}
+
+export default StateCard
---
+++ park-app-frontend/src/components/states/States.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class States extends React.Component {
+  render() {
+    <div>
+      States Component
+    </div>
+  }
+}
+
+export default States
