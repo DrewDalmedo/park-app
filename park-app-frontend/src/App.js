12e3af72113de9dc90e978afe81dfec23ee239bf
DrewDalmedo <andrewdalmedo@gmail.com>
2020-09-29 17:19:04 -0400

    Add imports for redux and react-redux

--- park-app-frontend/src/App.js
+++ park-app-frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import './App.css';
 
 import AddState from './components/states/AddState'
+
+
 
 function App() {
   return (
