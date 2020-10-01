c0043758ff799393aef058e60ba8b9830cac54f0
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-01 13:36:21 -0400

    Create ADD_STATE action for adding individual states

--- park-app-frontend/src/actions/ActionList.js
+++ park-app-frontend/src/actions/ActionList.js
@@ -1,6 +1,7 @@
 // states
 export const FETCH_STATES = 'FETCH_STATES'
 export const ADD_STATES = 'ADD_STATES'
+export const ADD_STATE = 'ADD_STATE'
 export const DELETE_STATE = 'DELETE_STATE'
 
 // parks
