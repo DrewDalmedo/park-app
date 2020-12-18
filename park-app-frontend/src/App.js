60957b3b39ca3ba4617416ab6e635f3622e961f0
DrewDalmedo <andrewdalmedo@gmail.com>
2020-12-18 10:39:44 -0500

    Import client side routing features and implement links to different pages

--- park-app-frontend/src/App.js
+++ park-app-frontend/src/App.js
@@ -5,23 +5,49 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import './App.css';
 
+// client side routing
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
 // reducers
 import StateReducer from './reducers/StateReducer'
 
 // components
 import AddState from './components/states/AddState'
 import States from './components/states/States'
+import Login from './components/users/Login'
 
 const store = createStore( StateReducer, applyMiddleware(thunk) )
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <h1> National Parks App </h1>
-        <AddState />
-        <States />
-      </div>
+      <Router>
+
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/">
+            <div>
+              <h1> National Parks App </h1>
+              <AddState />
+              <States />
+            </div>
+          </Route>
+
+        </Switch>
+
+      </Router>
     </Provider>
   );
 }
