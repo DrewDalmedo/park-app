1a4dba7d698784398030ceadb0df2718393365d0
DrewDalmedo <andrewdalmedo@gmail.com>
2020-12-18 11:18:55 -0500

    Create login page form and handle user inputs

--- park-app-frontend/src/components/users/Login.js
+++ park-app-frontend/src/components/users/Login.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
+
+
 
 const Login = () => {
+  const [username, setUsername] = useState("") 
+  const [password, setPassword] = useState("")
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(username)
+    console.log(password)
+  }
+
   return (
     <div>
-      Boilerplate is p cool
+      <form onSubmit={handleFormSubmit}>
+        <p>username</p>
+        <input type="text" onChange={handleUsernameChange} />
+        <p>password</p>
+        <input type="password" onChange={handlePasswordChange} />
+        <input type="submit" />
+      </form>
     </div>
   )
 }
