ebad5d6d01e8e506b177dfc613a0d28751e0eb74
DrewDalmedo <andrewdalmedo@gmail.com>
2021-01-18 12:17:48 -0500

    Create users controller

---
+++ park-app-backend/app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+  end
+end
