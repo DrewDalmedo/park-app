4b43afa8a5fe5756715932dee5c7d4ebdef03039
DrewDalmedo <andrewdalmedo@gmail.com>
2021-01-25 16:35:00 -0500

    Login automatically to the new user

--- park-app-backend/app/controllers/users_controller.rb
+++ park-app-backend/app/controllers/users_controller.rb
@@ -2,23 +2,34 @@ class UsersController < ApplicationController
   def index
     users = User.all
     render json: users
-    # add error handling later (status 500) 
+    # add json error handling later (status 500) 
   end
 
   def show
     user = User.find(params[:id])
-    # add error handling later (status 500)
+    # add json error handling later (status 500)
   end
 
   def create
     user = User.new(user_params)
-    # add error handling later (status 500)
+    if user.save
+      login!
+      render json: {
+        status: :created,
+        user: user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: user.errors.full_messages
+      }
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
-    # add error handling later (status 500)
+    # add json error handling later (status 500)
   end
 end
