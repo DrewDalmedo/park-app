3f67c6ef0cc69e4710c99e856a42426ce0dad248
DrewDalmedo <andrewdalmedo@gmail.com>
2021-01-25 15:41:53 -0500

    Add show method and permit user params

--- park-app-backend/app/controllers/users_controller.rb
+++ park-app-backend/app/controllers/users_controller.rb
@@ -2,14 +2,23 @@ class UsersController < ApplicationController
   def index
     users = User.all
     render json: users
+    # add error handling later (status 500) 
+  end
+
+  def show
+    user = User.find(params[:id])
+    # add error handling later (status 500)
   end
 
   def create
     user = User.new(user_params)
+    # add error handling later (status 500)
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:username, :email, :password)
+    # add error handling later (status 500)
   end
 end
