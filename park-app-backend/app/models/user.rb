ae8afafd0b5477cc33f3966267f5cee02379c610
DrewDalmedo <andrewdalmedo@gmail.com>
2021-01-25 15:35:42 -0500

    Add validations to user data

--- park-app-backend/app/models/user.rb
+++ park-app-backend/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  # uses bcrypt to encrypt passwords
   has_secure_password
+
+  # validations
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true # add custom validation of emails later (verifying correct format)
+
+
 end
